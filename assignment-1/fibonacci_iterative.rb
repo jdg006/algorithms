d75911553bf281d42700ce208f653beb4971ce15
Joe <jdg006@lvc.edu>
2017-11-17 21:01:43 +0000

    assignment 1 complete

--- assignment-1/fibonacci_iterative.rb
+++ assignment-1/fibonacci_iterative.rb
@@ -1,11 +1,13 @@
  def fib(n)
+    return 0 if n == 0
     fib_0 = 0
     fib_1 = 1
-    
-    (0..n-1).each do |i|
+    count = 0
+    while count < n-1
         temp = fib_0
-        fib_1 = fib_0
+        fib_0 = fib_1
         fib_1 = temp + fib_1
+        count += 1
     end
     return fib_1
  end
